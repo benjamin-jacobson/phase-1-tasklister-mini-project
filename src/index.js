3516d71658f824643eb9073bd0304c987894f317
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-05-31 01:47:57 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let f = document.querySelector('form')
+  
+  f.addEventListener('submit', (e) => {
+    e.preventDefault()
+    //console.log(e.target['new-task-description'].value)
+    buildToDo(e.target['new-task-description'].value)
+    f.reset() // resetting text box 
+  })
 });
+
+function buildToDo(todo){
+  // creating elements
+  let p = document.createElement('p')
+  let newButton = document.createElement('button')
+  newButton.addEventListener('click', handleDelete) // event listener on button
+  newButton.textContent = ' x'
+  p.textContent = `${todo} `
+  p.appendChild(newButton)
+  document.querySelector('#list').appendChild(p) // appending element
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove() // removing element if x clicked
+}
